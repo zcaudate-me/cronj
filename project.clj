f1a8d4ef6c98d01b425bf8cf0324a6c38b629617
zcaudate <z@caudate.me>
2012-07-28 10:42:45 +1000

    starting on the 1.0.0 branch

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cronj "0.1.0"
+(defproject cronj "1.0.0-SNAPSHOT"
   :description "This is task-scheduling library inspired by the cron syntax."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
