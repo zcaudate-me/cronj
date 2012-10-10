c088c101cff65ea832ed48030bce13ff4625b303
zcaudate <z@caudate.me>
2012-10-10 20:39:53 +1100

    Linking dependencies to hara 0.2.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cronj "0.3.1"
+(defproject cronj "0.3.3"
   :description "This is task-scheduling library inspired by the cron syntax."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.4"]
-                 [hara "0.1.0"]]               
+                 [hara "0.2.1"]]               
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}})
