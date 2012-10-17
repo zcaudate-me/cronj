c4f807777c83092586f29baa7c4211fa9df13a4f
zcaudate <z@caudate.me>
2012-10-17 13:31:06 +1100

    Internal overhaul. now at v0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cronj "0.4.1"
+(defproject cronj "0.5.2"
   :description "This is task-scheduling library inspired by the cron syntax."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
